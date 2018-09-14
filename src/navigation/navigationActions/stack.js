35830bedd8864581f98fc195c94401e0d95cfd3f
Long Nguyen <zcmgyu@hotmail.com>
2018-09-15 01:08:49 +0700

    Dummy for test

--- src/navigation/navigationActions/stack.js
+++ src/navigation/navigationActions/stack.js
@@ -1,59 +1,41 @@
 import { Navigation } from 'react-native-navigation';
-// import _ from 'lodash';
-// import { Colors } from '../../themes/index';
-// import { navigatorStyle } from '../navigatonStyle';
-
-// export function startStackScreen() {
-//   const ROOT_SCREEN = 'forgotPassword';
-//   // const ROOT_TITLE = 'home';
-//   Navigation.setRoot({
-//     root: {
-//       stack: {
-//         // options: _.merge(navigatorStyle, {
-//         //   topBar: {
-//         //     visibale: false,
-//         //     drawBehind: true,
-//         //     background: {
-//         //       color: Colors.secondary,
-//         //     },
-//         //   },
-//         // }),
-//         options: {
-//           topBar: {
-//             visible: true,
-//             drawBehind: true,
-//             animate: false,
-//           },
-//         },
-//         children: [
-//           {
-//             component: {
-//               name: ROOT_SCREEN,
-//               options: {
-//                 topBar: {
-//                   visible: false,
-//                   drawBehind: true,
-//                   animate: false,
-//                 },
-//               },
-//             },
-//           },
-//         ],
-//       },
-//     },
-//   });
-// }
+import _ from 'lodash';
+import { Colors } from '../../themes/index';
+import { navigatorStyle } from '../navigatonStyle';
 
 export function startStackScreen() {
-  const ROOT_SCREEN = 'forgotPassword';
+  const ROOT_SCREEN = 'intro';
   // const ROOT_TITLE = 'home';
   Navigation.setRoot({
     root: {
       stack: {
+        // options: _.merge(navigatorStyle, {
+        //   topBar: {
+        //     visibale: false,
+        //     drawBehind: true,
+        //     background: {
+        //       color: Colors.secondary,
+        //     },
+        //   },
+        // }),
+        options: {
+          topBar: {
+            visible: true,
+            drawBehind: true,
+            animate: false,
+          },
+        },
         children: [
           {
             component: {
               name: ROOT_SCREEN,
+              options: {
+                topBar: {
+                  visible: false,
+                  drawBehind: true,
+                  animate: false,
+                },
+              },
             },
           },
         ],
@@ -61,3 +43,21 @@ export function startStackScreen() {
     },
   });
 }
+
+// export function startStackScreen() {
+//   const ROOT_SCREEN = 'forgotPassword';
+//   // const ROOT_TITLE = 'home';
+//   Navigation.setRoot({
+//     root: {
+//       stack: {
+//         children: [
+//           {
+//             component: {
+//               name: ROOT_SCREEN,
+//             },
+//           },
+//         ],
+//       },
+//     },
+//   });
+// }
--- src/screens/User/SignIn.js
+++ src/screens/User/SignIn.js
@@ -1,11 +1,44 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import I18n from 'react-native-i18n';
+import { push } from '../../navigation/navigationActions';
+import { back } from '../../navigation/navigationButtons';
+import { styles as TextStyle } from '../../components/Text';
 
 class SignIn extends Component {
+  static options(passProps) {
+    return {
+      topBar: {
+        title: {
+          text: 'Sign In',
+        },
+        background: {
+          color: 'red',
+        },
+        largeTitle: {
+          visible: true,
+          ...TextStyle.largeTitle,
+        },
+        leftButtons: [back()],
+        drawBehind: true,
+      },
+    };
+  }
+
+  forgotPassword = () => {
+    push(this.props.componentId, 'forgotPassword', {
+      // title: I18n.t('forgotPassword'),
+    });
+  };
+
   render() {
-    return <View />;
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text onPress={this.forgotPassword}>forgotPassword</Text>
+      </View>
+    );
   }
 }
 SignIn.propTypes = {};
