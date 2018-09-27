5db4dc6aed061e998fc15fec29fb5e2e8ee785db
doan anh <anh.doan@enouvo.com>
2018-09-27 23:32:51 +0700

    search subjects

--- src/api/subjects.js
+++ src/api/subjects.js
@@ -1,6 +1,5 @@
 import { get } from './utils';
 
 export async function searchSubjects(data) {
-  console.log('data', data);
   return data === '' ? get('/subjects') : get(`/subjects?name=${data}`);
 }
--- src/redux/TutorsRedux/sagas.js
+++ src/redux/TutorsRedux/sagas.js
@@ -1,4 +1,41 @@
-import TutorsActions, { MODEL, IGNORE_ACTIONS } from './actions';
+import {
+  call, put, fork, takeLatest, select,
+} from 'redux-saga/effects';
+import { delay, cancel } from 'redux-saga';
+import _ from 'lodash';
+import TutorsActions, { MODEL, IGNORE_ACTIONS, TutorsTypes } from './actions';
 import rootCRUDSaga from '../crudCreator/saga';
+import { searchTutors } from '../../api/tutors';
+import { PRIMARY_KEY } from '../crudCreator/actions';
 
-export default [...rootCRUDSaga(MODEL, IGNORE_ACTIONS, TutorsActions)];
+function* handleSearchInput(text) {
+  yield delay(300);
+  try {
+    const response = yield call(searchTutors, text);
+    // if (response)
+    const { data } = yield select(state => state.tutors);
+    if (response.success) {
+      const convertData = {
+        ids: response.data.map(item => item[PRIMARY_KEY]),
+        data: { ...data, ..._.keyBy(response.data, PRIMARY_KEY) },
+      };
+      yield put(TutorsActions.getAllTutorsSuccess(convertData));
+    } else {
+      yield put(TutorsActions.getAllTutorsFailure(response));
+    }
+  } catch (err) {
+    yield put(TutorsActions.getAllTutorsFailure(err));
+  }
+}
+
+export function* searchTutorsSaga({ text }) {
+  let task = null;
+  if (task) {
+    yield cancel(task);
+  }
+  task = yield fork(handleSearchInput, text);
+}
+export default [
+  ...rootCRUDSaga(MODEL, IGNORE_ACTIONS, TutorsActions),
+  takeLatest(TutorsTypes.SEARCH_TUTOR, searchTutorsSaga),
+];
